735b420c31c4e9ccd8b8ef5962259a5aa4b5867e
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-03 18:23:05 -0300

    fix: change the jest.config import to the correctly extension

--- backend/jest-config-unit.cjs
+++ backend/jest-config-unit.cjs
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const config = require('./jest.config')
+const config = require('./jest.config.cjs')
 config.testMatch = ['**/*.spec.ts']
 module.exports = config
