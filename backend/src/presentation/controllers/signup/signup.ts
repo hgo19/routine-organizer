9d703862e12862f40ba897b4e3af6f346bd29550
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-05 16:29:23 -0300

    feat: ensure SignUp Controller calls AddAccountUseCase with the right values

--- backend/src/presentation/controllers/signup/signup.ts
+++ backend/src/presentation/controllers/signup/signup.ts
@@ -1,14 +1,14 @@
+import { type AddAccountUseCase } from '../../../domain/usecases/add-account'
+import { type AccountInput } from '../../../domain/usecases/add-account-protocols'
 import { type HttpResponse, type IController } from '../../protocols'
 
-export interface SignUpRequest {
-  name: string
-  email: string
-  password: string
-  passwordConfirmation: string
-}
+export class SignUpController implements IController<AccountInput> {
+  private readonly _useCase
+  constructor (addAccountUseCase: AddAccountUseCase) {
+    this._useCase = addAccountUseCase
+  }
 
-export class SignUpController implements IController<SignUpRequest> {
-  async execute (value: Partial<SignUpRequest>): Promise<HttpResponse> {
+  async execute (value: Partial<AccountInput>): Promise<HttpResponse> {
     const properties = ['name', 'email', 'password', 'passwordConfirmation']
 
     if (!properties.every((property) => property in value)) {
@@ -18,6 +18,12 @@ export class SignUpController implements IController<SignUpRequest> {
       }
       return await new Promise(resolve => { resolve(response) })
     }
+
+    const name = value.name as string
+    const email = value.email as string
+    const password = value.password as string
+    const passwordConfirmation = value.passwordConfirmation as string
+    await this._useCase.add({ name, email, password, passwordConfirmation })
 
     return await new Promise(resolve => {
       resolve({
