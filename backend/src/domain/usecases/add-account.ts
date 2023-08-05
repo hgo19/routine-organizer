bb2fc46d2e67cf35b5b565066c3730cf3af06312
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-05 17:02:57 -0300

    feat: ensure AddAccountUseCase calls userEntity.createUser with the correct values

--- backend/src/domain/usecases/add-account.ts
+++ backend/src/domain/usecases/add-account.ts
@@ -1,12 +1,19 @@
+import { type UserAccount } from '../entities/UserAccount'
 import { type AccountInput, type AccountModel } from './add-account-protocols'
 
 export class AddAccountUseCase {
+  private readonly userEntity
+  constructor (userEntity: UserAccount) {
+    this.userEntity = userEntity
+  }
+
   async add (account: AccountInput): Promise<AccountModel> {
+    this.userEntity.createUser(account)
     const fakeAccount = {
       id: 'valid_id',
       name: account.name,
       email: account.email,
-      password: account.password
+      password: 'hashed_password'
     }
 
     return await new Promise(resolve => { resolve(fakeAccount) })
