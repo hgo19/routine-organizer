9904ccd854e540a5393e7544febc55048bc1d459
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-06 15:15:41 -0300

    test: ensure an error throws in AddAccountUseCase when some troubles happens in business validations

--- backend/src/domain/usecases/add-account.spec.ts
+++ backend/src/domain/usecases/add-account.spec.ts
@@ -6,7 +6,28 @@ describe('', () => {
     // System under test
     const userEntity = new UserAccount()
     const sut = new AddAccountUseCase(userEntity)
-    const createUserSpy = jest.spyOn(userEntity, 'createUser')
+    const createUserSpy = jest.spyOn(userEntity, 'create')
+    const accountInput = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+      passwordConfirmation: 'valid_password'
+    }
+
+    await sut.add(accountInput)
+
+    expect(createUserSpy).toHaveBeenCalledWith(accountInput)
+  })
+
+  test('should throw an error if there is some troubles in business validations', async () => {
+    class UserEntityStub extends UserAccount {
+      validations (): boolean {
+        throw new Error()
+      }
+    }
+
+    const userEntity = new UserEntityStub()
+    const sut = new AddAccountUseCase(userEntity)
     const accountInput = {
       name: 'valid_name',
       email: 'valid_email',
@@ -14,8 +35,6 @@ describe('', () => {
       passwordConfirmation: 'invalid_password'
     }
 
-    await sut.add(accountInput)
-
-    expect(createUserSpy).toHaveBeenCalledWith(accountInput)
+    await expect(sut.add(accountInput)).rejects.toThrow()
   })
 })
