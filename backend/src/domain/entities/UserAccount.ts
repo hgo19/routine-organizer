2f13bd738ab3ae6a660224eaf7b9de88b1d30826
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-06 16:11:44 -0300

    feat: ensure the UserAccount entity show the right user informations when it was created on success

--- backend/src/domain/entities/UserAccount.ts
+++ backend/src/domain/entities/UserAccount.ts
@@ -1,26 +1,26 @@
 import { type AccountInput } from '../usecases/add-account-protocols'
 
 export class UserAccount {
-  private name: string
-  private email: string
-  private password: string
-  private passwordConfirmation: string
-  private readonly emailRgx: RegExp
+  private _name: string
+  private _email: string
+  private _password: string
+  private _passwordConfirmation: string
+  private readonly _emailRgx: RegExp
 
   constructor () {
-    this.name = ''
-    this.email = ''
-    this.password = ''
-    this.passwordConfirmation = ''
-    this.emailRgx = /\S+@\S+\.\S+/
+    this._name = ''
+    this._email = ''
+    this._password = ''
+    this._passwordConfirmation = ''
+    this._emailRgx = /\S+@\S+\.\S+/
   }
 
   create (userInfo: AccountInput): boolean {
     this.validations(userInfo)
-    this.name = userInfo.name
-    this.email = userInfo.email
-    this.password = userInfo.password
-    this.passwordConfirmation = userInfo.passwordConfirmation
+    this._name = userInfo.name
+    this._email = userInfo.email
+    this._password = userInfo.password
+    this._passwordConfirmation = userInfo.passwordConfirmation
     return true
   }
 
@@ -29,13 +29,13 @@ export class UserAccount {
     this.lengthValidations(userInfo)
 
     if (password !== passwordConfirmation) {
-      throw new Error('Invalid Parameters: Passwords must be the same!')
+      throw new Error('Invalid Parameters: _Passwords must be the same!')
     }
-    if (!this.emailRgx.test(email)) {
-      throw new Error('Invalid Parameters: Email is incorrect')
+    if (!this._emailRgx.test(email)) {
+      throw new Error('Invalid Parameters: _Email is incorrect')
     }
     const properties = [name, email, password, passwordConfirmation]
-    if (!properties.some((property) => property === '')) {
+    if (properties.some((property) => property.length === 0)) {
       throw new Error("Invalid Parameters: Property can't be empty")
     }
   }
@@ -43,11 +43,23 @@ export class UserAccount {
   lengthValidations (userInfo: AccountInput): void {
     const { name, password } = userInfo
     if (name.length < 3) {
-      throw new Error('Invalid Parameters: Name must be more than 3 characters')
+      throw new Error('Invalid Parameters: _Name must be more than 3 characters')
     }
 
     if (password.length < 6) {
-      throw new Error('Invalid Parameters: Password must be more than 6 characters')
+      throw new Error('Invalid Parameters: _Password must be more than 6 characters')
     }
+  }
+
+  get name (): string {
+    return this._name
+  }
+
+  get email (): string {
+    return this._email
+  }
+
+  get password (): string {
+    return this._password
   }
 }
