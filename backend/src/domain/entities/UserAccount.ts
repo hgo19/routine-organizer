1cc5d92f91ced835b059aa1ee146ef0a1f64cf8c
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-06 16:15:30 -0300

    refactor: creates an InvalidParamsError to throws when an invalid parameter was given

--- backend/src/domain/entities/UserAccount.ts
+++ backend/src/domain/entities/UserAccount.ts
@@ -1,3 +1,4 @@
+import { InvalidParamsError } from '../exceptions/invalid-params'
 import { type AccountInput } from '../usecases/add-account-protocols'
 
 export class UserAccount {
@@ -29,25 +30,25 @@ export class UserAccount {
     this.lengthValidations(userInfo)
 
     if (password !== passwordConfirmation) {
-      throw new Error('Invalid Parameters: _Passwords must be the same!')
+      throw new InvalidParamsError('asswords must be the same!')
     }
     if (!this._emailRgx.test(email)) {
-      throw new Error('Invalid Parameters: _Email is incorrect')
+      throw new InvalidParamsError('Email is incorrect')
     }
     const properties = [name, email, password, passwordConfirmation]
     if (properties.some((property) => property.length === 0)) {
-      throw new Error("Invalid Parameters: Property can't be empty")
+      throw new InvalidParamsError("Property can't be empty")
     }
   }
 
   lengthValidations (userInfo: AccountInput): void {
     const { name, password } = userInfo
     if (name.length < 3) {
-      throw new Error('Invalid Parameters: _Name must be more than 3 characters')
+      throw new InvalidParamsError('Name must be more than 3 characters')
     }
 
     if (password.length < 6) {
-      throw new Error('Invalid Parameters: _Password must be more than 6 characters')
+      throw new InvalidParamsError('Password must be more than 6 characters')
     }
   }
 
