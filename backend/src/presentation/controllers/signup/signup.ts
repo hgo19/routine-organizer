bdc6d742550642eb706ddcfc712ab665d751607c
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-14 16:30:17 -0300

    feat: signUp controller returns status code with error http if AddAccountUseCase throws

--- backend/src/presentation/controllers/signup/signup.ts
+++ backend/src/presentation/controllers/signup/signup.ts
@@ -1,3 +1,5 @@
+import { InvalidParamsError } from '../../../domain/exceptions/invalid-params'
+import { TypeError } from '../../../domain/exceptions/type'
 import { type AddAccountUseCase } from '../../../domain/usecases/add-account'
 import { type HttpResponse, type IController, type AccountInput } from './signup-protocols'
 
@@ -8,27 +10,45 @@ export class SignUpController implements IController<AccountInput> {
   }
 
   async execute (value: Partial<AccountInput>): Promise<HttpResponse> {
-    const properties = ['name', 'email', 'password', 'passwordConfirmation']
+    try {
+      const properties = ['name', 'email', 'password', 'passwordConfirmation']
 
-    if (!properties.every((property) => property in value)) {
-      const response = {
-        statusCode: 400,
-        body: {}
+      if (!properties.every((property) => property in value)) {
+        const response = {
+          statusCode: 400,
+          body: {}
+        }
+        return await new Promise(resolve => { resolve(response) })
       }
-      return await new Promise(resolve => { resolve(response) })
-    }
 
-    const name = value.name as string
-    const email = value.email as string
-    const password = value.password as string
-    const passwordConfirmation = value.passwordConfirmation as string
-    await this._useCase.add({ name, email, password, passwordConfirmation })
+      const name = value.name as string
+      const email = value.email as string
+      const password = value.password as string
+      const passwordConfirmation = value.passwordConfirmation as string
+      await this._useCase.add({ name, email, password, passwordConfirmation })
 
-    return await new Promise(resolve => {
-      resolve({
-        statusCode: 200,
-        body: {}
+      return await new Promise(resolve => {
+        resolve({
+          statusCode: 200,
+          body: {}
+        })
       })
-    })
+    } catch (error) {
+      if (error instanceof InvalidParamsError || error instanceof TypeError) {
+        return {
+          statusCode: 422,
+          body: {
+            message: error.message
+          }
+        }
+      }
+      console.log(error)
+      return {
+        statusCode: 500,
+        body: {
+          message: 'Internal Server Error'
+        }
+      }
+    }
   }
 }
