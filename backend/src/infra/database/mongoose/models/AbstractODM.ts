8a7dca1c5ad2b47d18d14f6ac5b46446e977c1af
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-21 15:59:40 -0300

    feat: create AbstractODM and UserODM

---
+++ backend/src/infra/database/mongoose/models/AbstractODM.ts
@@ -0,0 +1,43 @@
+import {
+  type Model,
+  type Schema,
+  model,
+  models,
+  type UpdateQuery,
+  isValidObjectId
+} from 'mongoose'
+
+abstract class AbstractODM<T> {
+  protected schema: Schema
+  protected model: Model<T>
+  protected modelName: string
+
+  constructor (schema: Schema, modelName: string) {
+    this.schema = schema
+    this.modelName = modelName
+    this.model = models[this.modelName] ?? model(this.modelName, this.schema)
+  }
+
+  public async create (entity: T): Promise<unknown> {
+    return await this.model.create({ ...entity })
+  }
+
+  public async getAll (): Promise<unknown> {
+    return await this.model.find({})
+  }
+
+  public async getById (id: string): Promise<T | null> {
+    if (!isValidObjectId(id)) throw Error('Invalid mongo id')
+    return await this.model.findById(id)
+  }
+
+  public async update (id: string, entity: T): Promise<unknown> {
+    if (!isValidObjectId(id)) throw Error('Invalid mongo id')
+
+    const filter = { _id: id }
+    const update = { ...entity }
+    return await this.model.findOneAndUpdate(filter, update as UpdateQuery<T>, { new: true })
+  }
+}
+
+export default AbstractODM
---
+++ backend/src/infra/database/mongoose/models/UserODM.ts
@@ -0,0 +1,20 @@
+import {
+  Schema
+} from 'mongoose'
+import { type AccountModel } from '../../../../domain/protocols'
+import AbstractODM from './AbstractODM'
+
+class UserODM extends AbstractODM<AccountModel> {
+  constructor () {
+    const schema = new Schema<AccountModel>({
+      name: { type: String, required: true },
+      email: { type: String, required: true },
+      password: { type: String, required: true }
+    }, {
+      versionKey: false // You should be aware of the outcome after set to false
+    })
+    super(schema, 'User')
+  }
+}
+
+export default UserODM
