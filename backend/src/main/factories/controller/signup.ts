067c491cb3758d0651658d312773aedcb674e499
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-22 17:27:49 -0300

    refactor: change signUp controller factory

--- backend/src/main/factories/controller/signup.ts
+++ backend/src/main/factories/controller/signup.ts
@@ -1,6 +1,4 @@
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
 import { makeAddAccount } from '../usecases/add-account'
 
-export const makeSignUp = (): SignUpController => {
-  return new SignUpController(makeAddAccount())
-}
+export const makeSignUpController = new SignUpController(makeAddAccount())
