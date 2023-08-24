cf4551f3aa1ecb5d6fae21ccaa546ad419ff8634
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-24 17:00:40 -0300

    refactor: change usecase type import in signup controller

--- backend/src/presentation/controllers/signup/signup.ts
+++ backend/src/presentation/controllers/signup/signup.ts
@@ -1,17 +1,16 @@
 import { type NextFunction, type Request, type Response } from 'express'
 import { type AddAccountUseCase } from '../../../domain/usecases/add-account'
-import { type IController } from './signup-protocols'
 
-export class SignUpController implements IController {
-  private readonly _useCase
+export class SignUpController {
+  private readonly useCase
   constructor (addAccountUseCase: AddAccountUseCase) {
-    this._useCase = addAccountUseCase
+    this.useCase = addAccountUseCase
   }
 
   execute = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try {
       const { name, email, password, passwordConfirmation } = req.body
-      const userCreated = await this._useCase.add({ name, email, password, passwordConfirmation })
+      const userCreated = await this.useCase.add({ name, email, password, passwordConfirmation })
 
       return res.status(201).json(userCreated)
     } catch (error) {
