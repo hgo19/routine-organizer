f926857f396ec4e68218c9d5425d0faa0c3d9d1c
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-24 15:42:07 -0300

    refactor: remove presentation protocols

--- backend/src/presentation/protocols/controller.ts
+++
@@ -1,5 +0,0 @@
-import { type NextFunction, type Request, type Response } from 'express'
-
-export interface IController {
-  execute: (req: Request, res: Response, next: NextFunction) => Promise<any>
-}
