98946086b230c3c9cf4a79f95ef11e5c1799162d
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-29 16:13:25 -0300

    feat: implements post/user/login route

--- backend/src/main/routes/user.routes.ts
+++ backend/src/main/routes/user.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import { makeSignUpController } from '../factories/controller/signup'
 import { userValidations } from '../../presentation/middlewares/signup-validations'
+import { makeLoginController } from '../factories/controller/login'
 
 const userRoute = Router()
 
 userRoute.post('/register', userValidations, makeSignUpController.execute)
+
+userRoute.post('/login', makeLoginController.execute)
 
 export default userRoute
