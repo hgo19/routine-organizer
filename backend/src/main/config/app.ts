f46aa3fb6accf207139f543a2979152b9ad25025
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-31 19:14:13 -0300

    feat: configure cors on the app

--- backend/src/main/config/app.ts
+++ backend/src/main/config/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import userRoute from '../routes/user.routes'
 import { errorHandler, errorTreatment } from '../../presentation/middlewares/error-handler'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
@@ -8,5 +9,6 @@ app.use(express.json())
 app.use('/user', userRoute)
 app.use(errorTreatment)
 app.use(errorHandler)
+app.use(cors())
 
 export default app
