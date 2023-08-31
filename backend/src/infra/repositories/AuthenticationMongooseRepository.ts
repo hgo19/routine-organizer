92fe5d3e83ff7087d4b02d8f8f23fbce9059a27f
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-31 19:57:26 -0300

    feat: throws error when can not find an user in Db by Email

--- backend/src/infra/repositories/AuthenticationMongooseRepository.ts
+++ backend/src/infra/repositories/AuthenticationMongooseRepository.ts
@@ -1,5 +1,6 @@
 import { type AccountModel } from '../../domain/protocols'
 import { type AuthenticationRepository } from '../../domain/protocols/authentication-repository'
+import { NotFoundError } from '../../presentation/helpers/http-erros'
 import UserODM from '../database/mongoose/models/UserODM'
 
 export class AuthenticationMongooseRepository implements AuthenticationRepository {
@@ -10,6 +11,8 @@ export class AuthenticationMongooseRepository implements AuthenticationRepositor
 
   async findByEmail (email: string): Promise<AccountModel> {
     const userInDb = await this.persistence.findByEmail(email)
+    if (userInDb == null) throw new NotFoundError("Can't find user in db")
+
     const accountDTO = {
       id: userInDb._id,
       name: userInDb.name,
