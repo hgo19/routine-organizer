36af976283a22157f6ddf655a20f0a2e94337efd
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-04 18:40:38 -0300

    feat: create Register and Dashboard page on Router

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,10 +1,15 @@
-import { Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Dashboard />} />
     </Routes>
   )
 }
