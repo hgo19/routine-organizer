7738bac1eb06e5ed90c58b03e32a3f9ce5ae2038
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-08 14:44:45 -0300

    feat: create Habits component

---
+++ frontend/src/components/Habits/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+export default function Habits() {
+  return (
+    <div className="habits-container">
+      <h2>Habits</h2>
+      <div className="habit-card">
+        <h4>Habit Example</h4>
+        <p>habit small description</p>
+      </div>
+      <div className="habit-card">+</div>
+    </div>
+  )
+}
